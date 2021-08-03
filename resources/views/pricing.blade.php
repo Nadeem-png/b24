1ba2d400450005865785ceba88d784f8fe69ba71
Nadeem-png <nk27890@gmail.com>
2021-08-03 07:51:12 -0700

    welcome page

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -1,8 +1,267 @@
 @extends('master/layout')
 @section('content')
+<style>
+.margin{
+    margin-top:-370px;
+}
+.div4{
+    background-image:url({{asset('image/datacenter-iceland.jpg')}});
+    background-repeat: no-repeat;
+        background-size: cover;
+        height:auto;
+        width:100%;
+        border-radius:10px;
+}
+.farm{
+    background-color:#2b2b2b;
+    border-radius:0 0 7px 7px;
+}
+.card1{
+    background-color:#2b2b2b;
+    border-radius: 7px;
+}
+
+    
+</style>
+  
+<div class="container container-sm margin text-center"  >
+<img src="" alt="">
+<h1 class="text-center text-white mb-4">Pricing</h1>
+<h5 class="text-color">Benefit from flexible pricing 
+according to your own wishes. Purchasing more hardware 
+power reduces the cost return periods and increases the amount of
+ Bitcoins you receive each month.</h5>
+</div>
+
+<div class="container container-sm mt-5">
+<h2 class="text-white text-center">Get more hardware power and pay less</h2>
+<div class="row mt-5">
+<div class="col-12 col-lg-4">
+<div class="card h-100 card1 p-4">
+  <div class="card-body">
+    <em><h6 class="text-center card-text text-color">MINIMUM POWER</h6></em>
+    <h2 class="text-center card-text text-white mt-2">6.64 TH/s</h2>
+    <h5 class="text-center card-text text-white mt-2">0.00090850 BTC</h5>
+    <p class="text-center card-text text-color border rounded-pill py-1  mt-2">1 TH/s = 0.00013680 BTC</p>
+  
+  <div class="row mt-5">
+  <div class="col-5">
+  <p class="text-color">BTC/Month</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">0.0023 BTC (+250%)</p>
+  </div>
+  </div>
+  
+  <div class="row mt-3">
+  <div class="col-5">
+  <p class="text-color">BTC/week</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">0.0005BTC(+58.3%)</p>
+  </div>
+  </div>
+  
+  <div class="row mt-3">
+  <div class="col-5">
+  <p class="text-color">BTC/Day</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">0.0001 BTC (+8.3%)</p>
+  </div>
+  </div>
+  
+  <div class="row mt-3">
+  <div class="col-5">
+  <p class="text-color">BTC/Hour</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">
+0.0000 BTC (+0.3%)</p>
+  </div>
+  </div>
+  
+  </div>
+  <div class="card-footer bg-white">
+Approximate return on cost: <strong>12 days</strong>
+
+  </div>
+</div>
+</div>
+
+<div class="col-12 col-lg-4">
+<div class="card h-100 card1 p-4">
+  <div class="card-body">
+    <em><h6 class="text-center card-text text-color">YOUR POWER</h6></em>
+    <h2 class="text-center card-text text-white mt-2">6.64 TH/s</h2>
+    <h5 class="text-center card-text text-white mt-2">0.00090850 BTC</h5>
+    <p class="text-center card-text text-color border rounded-pill py-1  mt-2">1 TH/s = 0.00013680 BTC</p>
+  
+  <div class="row mt-5">
+  <div class="col-5">
+  <p class="text-color">BTC/Month</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">0.0023 BTC (+250%)</p>
+  </div>
+  </div>
+  
+  <div class="row mt-3">
+  <div class="col-5">
+  <p class="text-color">BTC/week</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">0.0005BTC(+58.3%)</p>
+  </div>
+  </div>
+  
+  <div class="row mt-3">
+  <div class="col-5">
+  <p class="text-color">BTC/Day</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">0.0001 BTC (+8.3%)</p>
+  </div>
+  </div>
+  
+  <div class="row mt-3">
+  <div class="col-5">
+  <p class="text-color">BTC/Hour</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">
+0.0000 BTC (+0.3%)</p>
+  </div>
+  </div>
+  
+  </div>
+  <div class="card-footer bg-white">
+Approximate return on cost: <strong>12 days</strong>
+
+  </div>
+</div>
+</div>
 
 
+<div class="col-12 col-lg-4">
+<div class="card h-100 card1 p-4">
+  <div class="card-body">
+    <em><h6 class="text-center card-text text-color">MINIMUM POWER</h6></em>
+    <h2 class="text-center card-text text-white mt-2">6.64 TH/s</h2>
+    <h5 class="text-center card-text text-white mt-2">0.00090850 BTC</h5>
+    <p class="text-center card-text text-color border rounded-pill py-1  mt-2">1 TH/s = 0.00013680 BTC</p>
+  
+  <div class="row mt-5">
+  <div class="col-5">
+  <p class="text-color">BTC/Month</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">0.0023 BTC (+250%)</p>
+  </div>
+  </div>
+  
+  <div class="row mt-3">
+  <div class="col-5">
+  <p class="text-color">BTC/week</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">0.0005BTC(+58.3%)</p>
+  </div>
+  </div>
+  
+  <div class="row mt-3">
+  <div class="col-5">
+  <p class="text-color">BTC/Day</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">0.0001 BTC (+8.3%)</p>
+  </div>
+  </div>
+  
+  <div class="row mt-3">
+  <div class="col-5">
+  <p class="text-color">BTC/Hour</p>
+  </div>
+  <div class="col-7">
+  <p class="text-white">
+0.0000 BTC (+0.3%)</p>
+  </div>
+  </div>
+  
+  </div>
+  <div class="card-footer bg-white">
+Approximate return on cost: <strong>12 days</strong>
 
+  </div>
+</div>
+</div>
+
+</div>
+<div class="mt-5 text-center">
+<span class="text-white font-weight-bold ml-3">No maintenance or set-up fees</span>
+<span class="text-white font-weight-bold ml-3">No maintenance or set-up fees</span>
+<span class="text-white font-weight-bold ml-3">No maintenance or set-up fees</span>
+</div>
+
+</div>
+
+
+<div class="container mt-5 div4" >
+<div class="container">
+<h2 class="text-center py-4 text-white">Top technology with low electricity costs</h2>
+<div class="row">
+<div class="col-6 col-lg text-center text-white mt-4 mb-4">
+<h5 class="mb-3">12 nm</h5>
+<h5>Processor chip size</h5>
+</div>
+<div class="col-6 col-lg text-center text-white mt-4 mb-4">
+<h5 class="mb-3">118.5 TH/s</h5>
+<h5>Max Power per Device</h5>
+</div>
+<div class="col-6 col-lg text-center text-white mt-4 mb-4">
+<h5 class="mb-3">$ 0.04/kWh</h5>
+<h5>Electricity costs</h5>
+</div>
+<div class="col-6 col-lg text-center text-white mt-4 mb-4">
+<h5 class="mb-3">1200+</h5>
+<h5>Total Mining Devices</h5>
+</div>
+<div class="col-6 col-lg text-center text-white mt-4 mb-4">
+<h5 class="mb-3">3750 W</h5>
+<h5>Power Supply</h5>
+</div>
+</div>
+</div>
+
+</div>
+
+
+<div class="container container-sm mt-5">
+
+<h2 class="text-center text-white ">Hardware in the perfect environment</h2>
+<div class="row mt-5">
+<div class="col-12 col-lg-4 farm1 mb-3">
+<img src="{{asset('image/farm-1.jpg')}}" alt="farm1" class="image-fluid w-100">
+<h5 class="text-color py-4 px-4 text-center  farm">Thanks to Iceland's natural climate and weather conditions, we save up to 86% on 
+additional hardware cooling, which is cost effective for each individual user</h5>
+</div>
+
+<div class="col-12 col-lg-4 farm1 mb-3">
+<img src="{{asset('image/farm-2.jpg')}}" alt="farm2" class="image-fluid w-100">
+<h5 class="text-color py-4 px-4 text-center farm">The daily maintenance and constant 
+monitoring of our hardware guarantees a continuous and fail-safe mining
+ of new Bitcoins for you. In case of failures, each miner
+ is supported by a backup miner.</h5>
+</div>
+
+<div class="col-12 col-lg-4 farm1 mb-3">
+<img src="{{asset('image/farm-3.jpg')}}" alt="farm3" class="image-fluid w-100">
+<h5 class="text-color py-4 px-4 text-center farm">Kazakhstan offers us a low electricity 
+price, which is below $ 0.05/kWh and gives us the oppor
+expand mining hardware faster and further.</h5>
+</div>
+</div>
+</div>
 
 @endsection
 @section('title','Home')
