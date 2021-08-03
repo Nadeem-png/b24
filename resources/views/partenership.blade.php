13d8c4ddb56e3e467fc17a218b5fcb3c14152adf
Nadeem-png <nk27890@gmail.com>
2021-08-03 05:29:11 -0700

    welcome page

--- resources/views/partenership.blade.php
+++ resources/views/partenership.blade.php
@@ -4,9 +4,65 @@
 .div5{
     border-radius:7px;
 }
+.margin{
+    margin-top:-370px;
+}
+.per{
+    border-radius:10px;
+}
+.btn{
+    background-color:white;
+}
 </style>
-div.co
+<div class="container container-sm margin text-center"  >
+<img src="" alt="">
+<h1 class="text-center text-white mb-4">Partnership</h1>
+<h5 class="text-color">Increase your hardware power with additional commission by inviting new users. Help to spread the currency of the future and make it more accessible. Build your own network 
+over 3 levels and passively grow your daily Bitcoin earnings.
+Introducing our 3-tier invitation system.</h5>
+</div>
 
+
+<div class="container container-sm mt-5 py-4 ">
+<h2 class="text-center text-white">Introducing our 3-tier invitation system</h2>
+<div class="row mt-5">
+<div class="col-12 col-lg-4 mb-3 ">
+<span class="bg-white text-center p-3 font-weight-bold per ">7%</span>
+<h5 class="text-color mt-4">
+Get your personal invitation link from the account panel and invite new people directly.
+ For every resulting purchase of Hardware Power you will receive a commission.
+</h5>
+</div>
+<div class="col-12 col-lg-4 mb-3">
+<span class="bg-white text-center p-3  font-weight-bold per">3%</span>
+<h5 class="text-color mt-4">
+If your invited user now follows the same
+ example, you will receive further commission on the purchases made
+ by your invited member's invited user.
+</h5>
+</div>
+<div class="col-12 col-lg-4  mb-3">
+<span class="bg-white text-center p-3 font-weight-bold per ">1%</span>
+<h5 class="text-color mt-4">
+The same principle follows a third and final
+ benefiting you and all your invited users.
+</h5>
+</div>
+</div>
+</div>
+
+<div class="container container-sm">
+<div class="card py-2 text-card div5   inline" style=" background-color:#2b2b2b;">
+<div class="card-body ">
+<h2 class="card-text text-white">Sign up today for your personal partner link
+
+<a href="" class="btn ml-5 rounded-pill ">Sign Up</a>
+</h2>
+
+</div>
+
+</div>
+</div>
 
 <div class="container container-sm mt-5">
 <h2 class="text-center text-white">Unlimited expanding</h2>
@@ -19,7 +75,7 @@ div.co
 
 </div>
 
-<div class="col-12 col-lg-6 mt-5">
+<div class="col-12 col-lg-6 mt-5 mb-5">
 <h5 class="text-start text-color">
 Because our multiple-tiered system works almost passively, you only need to focus on direct referrals. Tier 2 and 3 are fully automated and offer you infinite commissions and the 
 opportunity to exponentially increase your hardware power in the long run.</h5>
