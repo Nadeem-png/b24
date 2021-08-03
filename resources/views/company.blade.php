f62da99edea5958916e1ea69fbbf920dbc60b85e
Nadeem-png <nk27890@gmail.com>
2021-08-03 08:50:54 -0700

    welcome page

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -1,5 +1,70 @@
 @extends('master/layout')
 @section('content')
+<style>
+.margin{
+    margin-top:-370px;
+}
+.question{
+    background-color:#2b2b2b;
+    border-radius: 7px;
+}
+</style>
+
+
+<div class="container container-sm margin text-center"  >
+<img src="" alt="">
+<h1 class="text-center text-white mb-4">Company</h1>
+<h5 class="text-color">Our mission is to promote the trade and use of
+ Bitcoins and to strengthen the establishment of Bitcoin as a currency
+  in its own right. What does it take? A simple and free solution to
+   generate Bitcoins for everyone and make them accessible.
+ And that is what we offer!</h5>
+</div>
+
+
+<div class=" question p-4 mt-5 text-center container container-sm">
+<h3 class="text-white">Quick help with frequently asked questions
+<a href="/help" class=" btn py-2 px-4 rounded-pill " style="background-color:white; color:black"><strong>?</strong> Help section</a>
+</h3>
+</div>
+
+
+<div class="container container-sm  text-center">
+<h2 class="font-weight-bold mt-5 mb-5 text-white">Our Representatives</h2>
+<div class="row mt-5">
+<div class="col-6 col-lg text-white mb-2">
+<img src="{{asset('image/cfo.jpg')}}" alt="" class="img-fluid rounded-circle">
+<h4 class="mt-3 mb-3">Susanne Mason</h4>
+<h6 class="mb-4">Chief Financial Officer</h6>
+<p class="text-color">Worked at 2 well-known crypto exchanges, gained insights into the crypto trading world</p>
+</div>
+<div class="col-6 col-lg text-white mb-2">
+<img src="{{asset('image/cto.jpg')}}" alt="" class="img-fluid rounded-circle">
+<h4 class="mt-3 mb-3">Nazar Petrosyan</h4>
+<h6 class="mb-4">Chief Technology Officer</h6>
+<p class="text-color">Fascinated by crypto technologies, gained experience at a strong competitor of ours</p>
+</div>
+<div class="col-6 col-lg text-white mb-2">
+<img src="{{asset('image/ceo.jpg')}}" alt="" class="img-fluid rounded-circle">
+<h4 class="mt-3 mb-3">Leon Gregory</h4>
+<h6 class="mb-4">CEO & Founder</h6>
+<p class="text-color">15 years of professional experience at a traditional bank is enough – it's time to establish modern technologies!</p>
+</div>
+<div class="col-6 col-lg text-white mb-2">
+<img src="{{asset('image/cf.jpg')}}" alt="" class="img-fluid rounded-circle">
+<h4 class="mt-3 mb-3">Gorden Barlow</h4>
+<h6 class="mb-4">Gorden Barlow</h6>
+<p class="text-color">PhD in computer science, expert for servers, hardware and large-scale networks</p>
+</div>
+<div class="col-6 col-lg text-white mb-2">
+<img src="{{asset('image/hm.jpg')}}" alt="" class="img-fluid rounded-circle">
+<h4 class="mt-3 mb-3">Anna Womack</h4>
+<h6 class="mb-4">Head of Communications</h6>
+<p class="text-color">Active communication with people is the key for lasting satisfaction and solving problems</p>
+</div>
+</div>
+
+</div>
 
 @endsection
 @section('title','Home')
--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -1,6 +1,122 @@
 @extends('master/layout')
 @section('content')
 
+<style>
+.margin{
+    margin-top:-370px;
+}
+.question{
+    background-color:#2b2b2b;
+    border-radius: 7px;
+}
+
+
+
+.border-1{
+    border-top:1px solid #919191;
+border-right:1px solid #919191;
+border-bottom:1px solid #919191;
+border-radius:0 7px 7px 0;
+
+}
+.border-2{
+border-left:1px solid #919191;
+border-top:1px solid #919191;
+border-bottom:1px solid #919191;
+border-radius:7px 0 0 7px;
+
+}
+.border-3{
+border-top:1px solid #919191;
+border-right:1px solid #919191;
+border-bottom:1px solid #919191;
+border-radius:0 7px 7px 0;
+
+}
+.border-4{
+border-top:1px solid #919191;
+border-left:1px solid #919191;
+border-bottom:1px solid #919191;
+border-radius:7px 0 0 7px;
+
+}
+</style>
+
+
+<div class="container container-sm margin text-center"  >
+<img src="" alt="">
+<h1 class="text-center text-white mb-4">Help</h1>
+<h5 class="text-color">Do you have questions or 
+are you unsure about certain topics? No problem!
+ We have listed the most frequently asked questions from our users below and hope that we 
+can help you with the answers.!</h5>
+</div>
+
+
+<div class=" question p-4 mt-5 text-center container container-sm">
+<h3 class="text-white">Additional help from our Team
+<a href="/company" class=" btn py-2 px-4 rounded-pill " style="background-color:white; color:black"><strong>?</strong> Contact Us</a>
+</h3>
+</div>
+
+
+
+
+<div class="container container-sm mt-5">
+<div class="row">
+
+<div class="col-12 col-lg-6  p-5 border-1 mb-5">
+<h3 class="text-white ">How do I get free hardware power?</h3>
+<h6 class="mt-3 text-color">Every user who registers by the end
+ of September will be credited with 850 GH/s of hardware power
+ immediately after signing up. Bitcoin mining starts immediately.</h6>
+</div>
+
+<div class="col-12 col-lg-6  p-5 border-2 mb-5">
+<h3 class="text-white ">How much hardware power can I buy?</h3>
+<h6 class="mt-3 text-color">We currently have no limit on the amount
+ of hardware power each user can have active. The only restrictions 
+ are on deposits for technical reasons. 
+There are the following limits:</h6>
+<h5 class="text-color">Minimum: 24.88 GBP</h5>
+<h5 class="text-color">Maximum: 43138.15 GBP</h5>
+</div>
+ 
+<div class="col-12 col-lg-6  p-5 border-3 mb-5">
+<h3 class="text-white ">Are there any fees or additional costs?</h3>
+<h6 class="mt-3 text-color">Our system and service has no 
+fees or extra costs. Neither when depositing nor when withdrawing.
+ The only fees you will incur are Bitcoin transaction fees, which 
+ are part of the Bitcoin technology and beyond our control. 
+ The transaction fees for withdrawals 
+are of course covered by us.</h6>
+</div>
+<div class="col-12 col-lg-6  p-5 border-4 mb-5">
+<h3 class="text-white ">How long is my hardware power available?</h3>
+<h6 class="mt-3 text-color">Your hardware power is available directly from 
+the receipt of payment forever without any time limit. In the event of a
+ failure of your assigned hardware, backup miners are automatically activated and 
+serve as a failsafe to prevent mining.</h6>
+</div>
+<div class="col-12 col-lg-6  p-5 border-1 mb-5">
+<h3 class="text-white ">How can I buy Hardware Power?</h3>
+<h6 class="mt-3 text-color">You can purchase new Hardware Power at any 
+time through the Account Panel. Log in with your account or create a new 
+account and send an amount of your choice to the specified Bitcoin address. 
+Alternatively, you can exchange mined Bitcoins directly
+ into new hardware power within seconds.</h6>
+</div>
+<div class="col-12 col-lg-6  p-5 border-2 mb-5">
+<h3 class="text-white ">How do I receive my mined Bitcoins?</h3>
+<h6 class="mt-3 text-color">You get your mined Bitcoins anytime,
+ whenever you want. Log in to your account and pay out the amount 
+ of Bitcoins you choose directly to the Bitcoin wallet you specify.
+  You can specify your Bitcoin wallet in the settings
+ or directly on the payout page.</h6>
+</div>
+</div>
+</div>
+
 @endsection
 @section('title','Home')
 @section('nava','active')
